9eb7672f8638e2212ed1a741c36f2694bc3ccebf
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-10 22:46:24 -0700

    Add files via upload

---
+++ C#102MinMaxSkip/Program.cs
@@ -0,0 +1,19 @@
+﻿string input;
+
+int num;
+double val;
+
+do
+{
+    Console.Write("Enter a value: ");
+    input = Console.ReadLine();
+
+
+    if (double.TryParse(input, out val))
+    {
+        break;
+    }
+    Console.WriteLine("That was not a valid value!");
+
+} while (true);
+
