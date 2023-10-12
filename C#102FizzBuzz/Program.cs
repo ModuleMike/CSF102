d5af2067b7e7a9d2c4de8460ba21dd6ae3d5c36c
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-11 23:19:01 -0700

    Add files via upload

---
+++ C#102FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿for (int i = 1; i <=100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+        continue;
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+        continue;
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
